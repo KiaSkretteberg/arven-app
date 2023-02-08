d5140ff58b0a6855d554a0e83e5ace0783a6345a
KiaSkretteberg <kia.skretteberg@canamtechnologies.com>
2023-02-08 16:54:04 -0700

    Updated the serial trigger

--- serial-trigger.sql
+++ serial-trigger.sql
@@ -1,16 +1,16 @@
 -- Must be run as "root", cannot be run as "db_mng_arven"
 DELIMITER $$
-CREATE TRIGGER generate_serial 
+CREATE TRIGGER GenerateSerial 
 BEFORE INSERT ON Devices FOR EACH ROW 
 BEGIN 
     declare serial_prefix char(5); 
     declare current_year int; 
     declare serial char(10); 
     declare id int; 
-    set @serial_prefix = 'rx-ar'; 
+    set @serial_prefix = 'RX-AR'; 
     set @current_year = YEAR(CURDATE()); 
     set @serial = CONCAT(@serial_prefix,@current_year,'-'); 
-    set @id = IFNULL((SELECT id FROM devices ORDER BY id DESC LIMIT 1),0) + 1; 
-    set NEW.serial = CONCAT(@serial,LPAD(@id, 4, 0)); 
+    set @id = IFNULL((SELECT DeviceID FROM Devices ORDER BY DeviceID DESC LIMIT 1),0) + 1; 
+    set NEW.SerialNum = CONCAT(@serial,LPAD(@id, 4, 0)); 
 END$$
 DELIMITER ;
