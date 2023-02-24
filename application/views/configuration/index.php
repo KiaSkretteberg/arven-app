9aa5f741bdbb87477900e56633c38301c33eaece
KiaSkretteberg <kia.skretteberg@canamtechnologies.com>
2023-02-23 23:50:17 -0700

    Fixed the navigation styling and started working on the configuration page

--- application/views/configuration/index.php
+++ application/views/configuration/index.php
@@ -3,11 +3,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $this->load->view('partial/header');
 ?>
-<section>
+<div class="form-field">
+    <label for="">Device Serial</label>
+    <input type="text" readonly value="<?=$devices[0]->SerialNum?>">
+</div>
+<div class="btn-holder">
+    <button>
+        <i class="fas fa-tram"></i>
+        <span>Locate Stairs</span>
+    </button>
+    <button>
+        <i class="fas fa-power-off"></i>
+        <span>Reboot Device</span>
+    </button>
+    <button>
+        <i class="fas fa-heartbeat"></i>
+        <span>Run Diagnostics</span>
+    </button>
+    <button>
+        <i class="fas fa-sync"></i>
+        <span>Factory Reset</span>
+    </button>
+</div>
+<form action="" method="POST" class="grid">
     <div class="form-field">
-        <label for="">Device Serial</label>
-        <input type="text" readonly value="<?=$devices[0]->SerialNum?>">
+        <label for="">Timezone</label>
+        <select name="timezone">
+            <?php foreach($timezones as $timezone):?>
+                <option value="<?=$timezone?>"<?=$timezone == ($this->input->post("timezone") ? $this->input->post("timezone") : $default_timezone) ? ' selected' : ''?>><?=$timezone?></option>
+            <?php endforeach;?>
+        </select>
+    </div>   
+
+    <div class="form-field">
+        <label for="">Your Email *</label>
+        <input type="email" name="email" placeholder="e.g. john.smith@example.com" required value="<?=$this->input->post("email")?>">
+    </div> 
+
+    <div class="form-field">
+        <label for="">Your First Name *</label>
+        <input type="text" name="first_name" placeholder="e.g. John" required value="<?=$this->input->post("first_name")?>">
     </div>
-</section>
+
+    <div class="form-field">
+        <label for="">Your Last Name</label>
+        <input type="text" name="last_name" placeholder="e.g. Smith" value="<?=$this->input->post("last_name")?>">
+    </div>
+
+    <button type="submit">Update</button>
+</form>
 
 <?php $this->load->view('partial/footer'); ?>
