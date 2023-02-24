84c5c20d27e61c170ab8315215aef86e01bb79c0
KiaSkretteberg <kia.skretteberg@canamtechnologies.com>
2023-02-23 22:42:28 -0700

    Adding some styles for the filters

--- application/views/configuration/initial.php
+++ application/views/configuration/initial.php
@@ -9,28 +9,40 @@ $this->load->view('partial/header', array("exclude_header" => true));
     </p>
 </div>
 <form action="" method="POST" class="grid">
-    <label for="">Device Serial *</label>
-    <input type="text" name="serial" placeholder="e.g. RX-AR2023-XXXX" required value="<?=$this->input->post("serial")?>">
+    <div class="form-field">
+        <label for="">Device Serial *</label>
+        <input type="text" name="serial" placeholder="e.g. RX-AR2023-XXXX" required value="<?=$this->input->post("serial")?>">
+    </div>  
 
-    <label for="">Timezone</label>
-    <select name="timezone">
-        <?php foreach($timezones as $timezone):?>
-            <option value="<?=$timezone?>"<?=$timezone == ($this->input->post("timezone") ? $this->input->post("timezone") : $default_timezone) ? ' selected' : ''?>><?=$timezone?></option>
-        <?php endforeach;?>
-    </select>
+    <div class="form-field">
+        <label for="">Timezone</label>
+        <select name="timezone">
+            <?php foreach($timezones as $timezone):?>
+                <option value="<?=$timezone?>"<?=$timezone == ($this->input->post("timezone") ? $this->input->post("timezone") : $default_timezone) ? ' selected' : ''?>><?=$timezone?></option>
+            <?php endforeach;?>
+        </select>
+    </div>    
 
-    <label for="">Your First Name *</label>
-    <input type="text" name="first_name" placeholder="e.g. John" required value="<?=$this->input->post("first_name")?>">
+    <div class="form-field">
+        <label for="">Your First Name *</label>
+        <input type="text" name="first_name" placeholder="e.g. John" required value="<?=$this->input->post("first_name")?>">
+    </div>
 
-    <label for="">Your Last Name</label>
-    <input type="text" name="last_name" placeholder="e.g. Smith" value="<?=$this->input->post("last_name")?>">
+    <div class="form-field">
+        <label for="">Your Last Name</label>
+        <input type="text" name="last_name" placeholder="e.g. Smith" value="<?=$this->input->post("last_name")?>">
+    </div>
 
-    <label for="">Your Email *</label>
-    <input type="email" name="email" placeholder="e.g. john.smith@example.com" required value="<?=$this->input->post("email")?>">
+    <div class="form-field">
+        <label for="">Your Email *</label>
+        <input type="email" name="email" placeholder="e.g. john.smith@example.com" required value="<?=$this->input->post("email")?>">
+    </div>
 
-    <label for="">Login Password *</label>
-    <input type="password" name="password" placeholder="*****" required value="<?=$this->input->post("password")?>">
-
+    <div class="form-field">
+        <label for="">Login Password *</label>
+        <input type="password" name="password" placeholder="*****" required value="<?=$this->input->post("password")?>">
+    </div>
+    
     <button type="submit">Let's Go!</button>
 
     <p>Already have an account? <a href="/login">Login</a> instead.</p>
--- application/views/history/index.php
+++ application/views/history/index.php
@@ -3,5 +3,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $this->load->view('partial/header');
 ?>
+<div class="form-field">
+    <label for=""></label>
+    <input type="text" placeholder="Enter a value to search by..." name="search">
+</div>
+
+<div class="btn-holder">
+    <div class="filters">
+        <button class="add-filter">Filter +</button>
+        <?php foreach($filters as $filter):?>
+            <button>
+                <span><?=$filter?></span>
+                <span>x</span>
+            </button>
+        <?php endforeach;?>
+        <!-- TODO: Remove this is just test for styling -->
+        <button class="filter">
+            <span>Test Filter 1</span>
+            <span>x</span>
+        </button>
+        <button class="filter">
+            <span>Test Filter 2</span>
+            <span>x</span>
+        </button>
+        <button class="filter">
+            <span>Test Filter 3</span>
+            <span>x</span>
+        </button>
+        <button class="filter">
+            <span>Test Filter 1</span>
+            <span>x</span>
+        </button>
+        <button class="filter">
+            <span>Test Filter 2</span>
+            <span>x</span>
+        </button>
+        <button class="filter">
+            <span>Test Filter 3</span>
+            <span>x</span>
+        </button>
+    </div>
+</div>
+
+<section class="full"></section>
 
 <?php $this->load->view('partial/footer'); ?>
