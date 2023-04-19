e7a3c3855396895b63d474f919ef0354e956d407
Sam <90291910+ssuitor@users.noreply.github.com>
2023-04-19 11:20:29 -0600

    first attempt at history update

--- application/controllers/History.php
+++ application/controllers/History.php
@@ -11,7 +11,29 @@ class History extends Site_Controller
 	public function index()
 	{
 		// TODO: we need to pull the EventLogs with most recent first
-
+		$options = array(
+			'userID'=> $this->session->UserID,
+			'order'=> 'desc'
+		);
 		// TODO: we need a list of filters that could be applied to EventLogs
+		// before date
+		if(/* before filter*/)
+		{
+				array_push($options, array('before' => $before));
+		}
+		//after date
+		if(/* after filter*/)
+		{
+					array_push($options, array('after' => $after));
+		}
+		// creator
+		if(/* creator filter*/)
+			{
+					array_push($options, array('maker' => $maker));
+			}
+		
+		
 	}
+
+	
 }
